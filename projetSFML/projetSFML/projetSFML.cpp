01964cb988841b247e6efd694b8f80b6eecc2e9f
Freddy <freddy_maupou@orange.fr>
2021-11-08 18:37:13 +0100

    Projet complètement initialisée

--- projetSFML/projetSFML/projetSFML.cpp
+++ projetSFML/projetSFML/projetSFML.cpp
@@ -2,9 +2,35 @@
 #include "windows.h"
 #include <iostream>
 
+std::string getAppPath() {
+	char cExeFilePath[256];
+	GetModuleFileNameA(NULL, cExeFilePath, 256);
+	std::string exeFilePath = cExeFilePath;
+	int exeNamePos = exeFilePath.find_last_of("\\/");
+	std::string appPath = exeFilePath.substr(0, exeNamePos + 1);
+	return appPath;
+}
+
+std::string getAssetPath() {
+	return getAppPath() + "\\Assets";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	sf::RenderWindow window(sf::VideoMode(800, 600), "ChronoSpacer");
+	// Initialise everything below
+	// Game loop
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			// Process any input event here
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		window.clear();
+		// Whatever I want to draw goes here
+		window.display();
+	}
 }
 
