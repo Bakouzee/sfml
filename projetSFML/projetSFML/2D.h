1a95cd6b5f09055eef8fe7a871ee4a61cae29661
Outfoxeed <letourneaumorigan@gmail.com>
2021-11-09 01:11:42 +0100

    Vector2D operators, constructor and GetDistance static method

--- projetSFML/projetSFML/2D.h
+++ projetSFML/projetSFML/2D.h
@@ -4,12 +4,37 @@
 struct Vector2
 {
 public:
-	float x;
-	float y;
+	float x = 0;
+	float y = 0;
 
-	std::string ToString() {
-		return "{" + std::to_string(x) + ", " + std::to_string(y) + "}";
-	}
-};
+	// Operators
+		// Plus
+	Vector2 operator+(Vector2 vec);
+	Vector2 operator+=(Vector2 vec);
+		// Minus
+	Vector2 operator-(Vector2 vec);
+	Vector2 operator-=(Vector2 vec);
+		// Multiply
+	Vector2 operator*(float multiplier);
+	Vector2 operator*=(float multiplier);
+	Vector2 operator*(Vector2 vec);
+	Vector2 operator*=(Vector2 vec);
+		// Divide
+	Vector2 operator/(float divider);
+	Vector2 operator/=(float divider);
+	Vector2 operator/(Vector2 vec);
+	Vector2 operator/=(Vector2 vec);
 
-float GetDistance(Vector2 a, Vector2 b);
+		// Assign
+	::Vector2& operator=(Vector2 vec);
+
+	// Constructors
+	Vector2();
+	Vector2(float x, float y);
+
+	static float GetDistance(Vector2 a, Vector2 b);
+	std::string ToString();
+}; 
+
+// std::ostream.operator<< variant working with Vector2
+std::ostream& operator<<(std::ostream& os, const Vector2 vec);
